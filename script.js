2b3865484948b59be0664b829132ec788d27e9fa
Ubuntu <ubuntu@ip-172-31-38-127.ap-south-1.compute.internal>
2026-05-24 07:06:51 +0000

    all file add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
     document.getElementById("message").innerHTML =
-        "Welcome to Bharrat Puri's DevOps Portfolio 🚀";
+        "Welcome to Akash Bajgire DevOps Portfolio 🚀";
 }
